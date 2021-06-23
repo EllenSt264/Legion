8849f04772c850e9ddf1a486103bd3eead4844bb
EllenSt264 <stafford.ellen@protonmail.com>
2021-06-23 15:18:27 +0100

    Fix Profile sidenav
    Add a translate attribute (like the one used in the Materialize sidenav) so that it does not interfere with other elements when inactive.

--- static/js/base.js
+++ static/js/base.js
@@ -18,12 +18,14 @@ $(document).ready(function () {
             onOpenStart: function () {
                 $('#profile-sidenav').css({
                     opacity: 1,
+                    transform: 'translateX(0%)',
                     transition: 'opacity 0.3s ease'
                 });
             },
             onCloseStart: function () {
                 $('#profile-sidenav').css({
                     opacity: 0,
+                    transform: 'translateX(-105%)'
                 });
             }
         });
