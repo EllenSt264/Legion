8dbadb82262b5ef356f8c8de4ec5d0e9d223537a
EllenSt264 <stafford.ellen@protonmail.com>
2021-06-23 14:47:39 +0100

    Add Profile mobile sidenav

--- static/js/base.js
+++ static/js/base.js
@@ -1,7 +1,31 @@
 $(document).ready(function () {
+    // Fullscreen overlay for service categories
     $(function () {
         $('.toggle-overlay').click(function () {
             $('aside').toggleClass('open');
         });
     });
+
+    var profileSidenav = true;
+
+    if (profileSidenav) {
+        // Fix sidenav aligment
+        $('.sidenav > li').css({
+            marginLeft: '50px'
+        });
+        // Trigger profile sidenav
+        $('.sidenav').sidenav({
+            onOpenStart: function () {
+                $('#profile-sidenav').css({
+                    opacity: 1,
+                    transition: 'opacity 0.3s ease'
+                });
+            },
+            onCloseStart: function () {
+                $('#profile-sidenav').css({
+                    opacity: 0,
+                });
+            }
+        });
+    }
 });
