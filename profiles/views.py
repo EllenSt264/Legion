0cceca094daafcb2aae46832ad67043bbfe5c169
EllenSt264 <stafford.ellen@protonmail.com>
2021-07-02 19:21:13 +0100

    Add Get Started pages for users to setup their profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def start(request):
+    """ A view to render the get started page """
+    return render(request, 'profiles/get-started.html')
+
+
+def start_creator(request):
+    """ A view to render the get started page """
+    return render(request, 'profiles/creator-get-started.html')
+
+
+def start_client(request):
+    """ A view to render the get started page """
+    return render(request, 'profiles/client-get-started.html')
