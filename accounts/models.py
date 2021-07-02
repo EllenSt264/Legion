049c8fa89a173f033c623b291abffef34ed941e6
EllenSt264 <stafford.ellen@protonmail.com>
2021-07-02 17:27:07 +0100

    Add identifier to custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -84,6 +84,12 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.email
 
+    def get_full_name(self):
+        return f'{self.first_name} {self.last_name}'
+
+    def get_short_name(self):
+        return self.first_name
+
     def has_perm(self, perm, obj=None):
         """ Does the user have a specific permission? """
         return True
