b5568cb14588e80449f0a3b179c50aaeddaa8656
EllenSt264 <stafford.ellen@protonmail.com>
2021-07-06 19:43:32 +0100

    Update UserProfile model and add to admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,46 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import UserProfile
+from .forms import UserProfileForm
+
+
+class UserProfileChangeForm(forms.ModelForm):
+    """ A form for updating users. Includes all fields but replaces
+     the password field with admin's disabled password hash display field. """
+
+    class Meta:
+        model = UserProfile
+        fields = ('user', 'title', 'overview', 'image', 'is_recruiter', 'is_creator')
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    # The forms to add and change user instances
+    form = UserProfileChangeForm
+    add_form = UserProfileForm
+
+    # The fields to be used in displaying the User model.
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    list_display = ('user', 'is_recruiter', 'is_creator')
+    fieldsets = (
+        ('Personal info', {'fields': ('user', 'title',
+                                      'overview', 'image',)}),
+        ('Permissions', {'fields': ('is_recruiter', 'is_creator')}),
+    )
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('user', 'title', 'overview', 'image',
+                       'is_recruiter', 'is_creator',),
+        }),
+    )
+    search_fields = ('user',)
+    ordering = ('user',)
+    filter_horizontal = ()
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- profiles/models.py
+++ profiles/models.py
@@ -1,26 +1,47 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth import get_user_model
-from django.http import request
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-
-User = get_user_model()
+from django_countries.fields import CountryField
+User = settings.AUTH_USER_MODEL
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    """ A class model for creating a user profile, which
+    shall extend the custom user model defined in the accounts app """
 
-    def __str__(self):
-        return self.user.email
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
 
+    title = models.CharField(max_length=50, null=True, blank=True, default='')
+    overview = models.TextField(null=True, blank=True, default='')
+    image = models.ImageField(null=True, blank=True)
 
-class UserRecruiter(models.Model):
-    """ A user profile model for users who will use the site as recruiters """
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    is_client = models.BooleanField(default=True)
     is_recruiter = models.BooleanField(default=False)
-    company_name = models.CharField(max_length=80, null=True, blank=True)
+    is_creator = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user.email
+
+
+class Recruiter(models.Model):
+    """ A class model for the recruiter user type """
+
+    company_name = models.CharField(max_length=70, null=False, blank=False)
+
+    town_or_city = models.CharField(max_length=70, null=False, blank=False)
+    postcode = models.CharField(max_length=70, null=False, blank=False)
+    country = CountryField(blank_label='Select Country', null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def update_profile_signal(sender, instance, created, **kwargs):
+    """ A signal to update or create the user profile """
+    user = instance
+    if created:
+        profile = UserProfile(user=user)
+        profile.save()
