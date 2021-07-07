6b1e880eb257aeef4c9cfa8b0eea98d85c94c957
EllenSt264 <stafford.ellen@protonmail.com>
2021-07-07 17:33:33 +0100

    Add Creator profile form template, url and view

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path('get-started/', views.start, name='start'),
     path('get-started/creator', views.start_creator, name='start_creator'),
+    path('get-started/creator/<int:user_id>', views.creator_form, name='creator_form'),
     path('get-started/client', views.start_client, name='start_client'),
     path('get-started/client/success', views.success_client, name='success_client'),
     path('get-started/creator/success', views.success_creator, name='success_creator'),
--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
@@ -16,6 +17,20 @@ def start_creator(request):
     """ A view to render the get started page """
 
     return render(request, 'profiles/creator-get-started.html')
+
+
+@login_required
+def creator_form(request, user_id):
+    """ A view to render the creator form """
+
+    user = get_object_or_404(get_user_model(), pk=user_id)
+
+    template = 'profiles/creator-form.html'
+    context = {
+        'user': user,
+    }
+
+    return render(request, template, context)
 
 
 @login_required
