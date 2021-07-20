bc66347598c9010bd89e6cb14bd9b19c8f68d158
EllenSt264 <stafford.ellen@protonmail.com>
2021-07-20 17:41:44 +0100

    Add Freelance Service to the admin

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import FreelanceService
 
-# Register your models here.
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'service_category',
+        'service_subcategory',
+        'user',
+        'service_headline',
+        'service_search_tags'
+    )
+
+    ordering = ('service_category',)
+
+
+admin.site.register(FreelanceService, ServiceAdmin)
