23bf879ae8909100e182e02826827eddd5725a6b
EllenSt264 <stafford.ellen@protonmail.com>
2021-07-22 18:40:37 +0100

    Update unit tests for Services app

--- services/tests.py
+++ services/tests.py
@@ -24,8 +24,7 @@ class AddServiceTest(TestCase):
         # Test Service model
         service = FreelanceService.objects.create(
             user=user,
-            service_category='Test Category',
-            service_subcategory='Test Subcategory',
+            category_name='WEB',
             service_headline='Test headline',
             service_search_tags='Hey,there',
             service_description='Testing the service description',
@@ -70,8 +69,7 @@ class AddServiceTest(TestCase):
         # Test Service model
         service = FreelanceService.objects.create(
             user=user,
-            service_category='Test Category',
-            service_subcategory='Test Subcategory',
+            category_name='WEB',
             service_headline='Test headline',
             service_search_tags='Hey,there',
             service_description='Testing the service description',
@@ -102,29 +100,28 @@ class AddServiceTest(TestCase):
         """ Test freelance service form """
 
         data = {
-            'service_category': 'Test Category',
-            'service_subcategory': 'Test Subcategory',
+            'category_name': 'WEB',
             'service_headline': 'Test headline',
-            'service_search_tags': 'Hey,there',
+            'service_search_tags': 'testing',
             'service_description': 'Testing the service description',
             'enable_all_packages': True,
-            'faq_question': 'Can you work with Django?',
-            'faq_answer': 'Yes, I also do manual testing and document my findings.',
+            'faq_question': '',
+            'faq_answer': '',
             'basic_package_title': 'Here is the basic package in a few words',
             'basic_delivery_time': 1,
             'basic_revisions': 1,
             'basic_details': 'Here are is additional information',
             'basic_price': 15,
-            'standard_package_title': 'Here is the standard package in a few words',
-            'standard_delivery_time': 1,
-            'standard_revisions': 1,
-            'standard_details': 'Here are is additional information',
-            'standard_price': 15,
-            'premium_package_title': 'Here is the premium package in a few words',
-            'premium_delivery_time': 1,
-            'premium_revisions': 1,
-            'premium_details': 'Here are is additional information',
-            'premium_price': 15,
+            'standard_package_title': '',
+            'standard_delivery_time': None,
+            'standard_revisions': None,
+            'standard_details': '',
+            'standard_price': None,
+            'premium_package_title': '',
+            'premium_delivery_time': None,
+            'premium_revisions': None,
+            'premium_details': '',
+            'premium_price': None,
             'service_image': '',
         }
 
@@ -136,8 +133,7 @@ class AddServiceTest(TestCase):
         with optional fields """
 
         data = {
-            'service_category': 'Test Category',
-            'service_subcategory': 'Test Subcategory',
+            'category_name': 'WEB',
             'service_headline': 'Test headline',
             'service_search_tags': 'Hey,there',
             'service_description': 'Testing the service description',
@@ -169,3 +165,72 @@ class AddServiceTest(TestCase):
 
         form = FreelanceServiceForm(data)
         self.assertTrue(form.is_valid())
+
+    def test_create_multiple_models_with_same_user(self):
+        # Create user
+        User = get_user_model()
+        user = User.objects.create(
+            first_name='Jon',
+            last_name='Smith',
+            email='test@mail.com',
+            date_of_birth='1998-01-01',
+            phone_number='+441234567890',
+            password='secret',
+        )
+
+        # Test Service model
+        service1 = FreelanceService.objects.create(
+            user=user,
+            category_name='WEB',
+            service_headline='Test headline',
+            service_search_tags='Hey,there',
+            service_description='Testing the service description',
+            enable_all_packages=False,
+            faq_question='Can you work with Django?',
+            faq_answer='Yes, I also do manual testing and document my findings.',
+            basic_package_title='Here is the basic package in a few words',
+            basic_delivery_time=1,
+            basic_revisions=1,
+            basic_details='Here are is additional information',
+            basic_price=15,
+            standard_package_title='',
+            standard_delivery_time=None,
+            standard_revisions=None,
+            standard_details='',
+            standard_price=None,
+            premium_package_title='',
+            premium_delivery_time=None,
+            premium_revisions=None,
+            premium_details='',
+            premium_price=None,
+            service_image='',
+        )
+        service2 = FreelanceService.objects.create(
+            user=user,
+            category_name='WEB',
+            service_headline='Test headline',
+            service_search_tags='Hey,there',
+            service_description='Testing the service description',
+            enable_all_packages=False,
+            faq_question='Can you work with Django?',
+            faq_answer='Yes, I also do manual testing and document my findings.',
+            basic_package_title='Here is the basic package in a few words',
+            basic_delivery_time=1,
+            basic_revisions=1,
+            basic_details='Here are is additional information',
+            basic_price=15,
+            standard_package_title='',
+            standard_delivery_time=None,
+            standard_revisions=None,
+            standard_details='',
+            standard_price=None,
+            premium_package_title='',
+            premium_delivery_time=None,
+            premium_revisions=None,
+            premium_details='',
+            premium_price=None,
+            service_image='',
+        )
+        service1.save()
+        service2.save()
+        self.assertEqual(FreelanceService.objects.count(), 2)
