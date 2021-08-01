451c2a9887b03f9faa3934f6c181cf1054ad0da6
EllenSt264 <stafford.ellen@protonmail.com>
2021-08-01 01:14:11 +0100

    Update Home view to render a different template
    Render template for the default landing pages for when authenticated users are logged in, and the default index.html for when a user is not logged in.

--- home/views.py
+++ home/views.py
@@ -1,6 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render, get_object_or_404
 
-# Create your views here.
+from profiles.models import UserProfile
+
+
 def home(request):
-    """ A view to render the homepage """
-    return render(request, 'home/index.html')
+    """ A view to render the home page """
+
+    if request.user.is_authenticated:
+        profile = get_object_or_404(UserProfile, user=request.user)
+        context = {
+            'profile': profile,
+        }
+        template = 'home/index-authenticated.html'
+        return render(request, template, context)
+    else:
+        return render(request, 'home/index.html')
