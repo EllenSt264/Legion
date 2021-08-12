3476058fd831153f5857fd0a60b00ddc857188e8
EllenSt264 <stafford.ellen@protonmail.com>
2021-08-12 02:14:38 +0100

    Update profiles context processor

--- profiles/context_processors.py
+++ profiles/context_processors.py
@@ -1,11 +1,23 @@
 from django.shortcuts import get_object_or_404
 
-from .models import UserProfile
+from .models import Creator, Recruiter, UserProfile
 
 
 def profile_processor(request):
     if request.user.is_authenticated:
         profile = get_object_or_404(UserProfile, user=request.user)
+        if profile.is_creator:
+            profile_contact = get_object_or_404(Creator, profile=profile)
+        elif profile.is_recruiter:
+            profile_contact = get_object_or_404(Recruiter, profile=profile)
+        else:
+            profile_contact = None
     else:
         profile = None
-    return {'profile': profile}
+        profile_contact = None
+
+    context = {
+        'profile': profile,
+        'profile_contact': profile_contact,
+    }
+    return context
