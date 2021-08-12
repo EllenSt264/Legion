d0571e1b9e2d8bf3dae82e2459066d11399f22e7
EllenSt264 <stafford.ellen@protonmail.com>
2021-08-12 22:32:06 +0100

    Add class methods to webhook handler and view

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 from . import views
+from .webhooks import webhook
 
 
 urlpatterns = [
     path('<int:service_id>/', views.checkout, name='checkout'),
     path('<int:service_id>/payment', views.checkout_payment, name='checkout_payment'),
     path('success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('wh/', webhook, name="webhook"),
 ]
--- legion/settings.py
+++ legion/settings.py
@@ -206,3 +206,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
